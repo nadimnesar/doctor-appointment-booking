a3f9a609e779987d2a1864eb18a65e83e406c5c9
Nesar Ahmed <neasr15-13636@diu.edu.bd>
2023-05-29 01:12:17 +0600

    sql

--- drsappoint_db.sql
+++ drsappoint_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2022 at 08:53 AM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Generation Time: May 28, 2023 at 09:11 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -43,11 +43,12 @@ CREATE TABLE `dr_list` (
 --
 
 INSERT INTO `dr_list` (`id`, `name`, `img_id`, `specialty`, `division`, `district`, `day`, `money`) VALUES
-(1, 'Sofia Brient', '1', 'Child Specialist', 'Dhaka', 'Dhaka', 'Friday', 500),
-(2, 'Karen Ray', '2', 'Skin Specialist', 'Chittagong', 'Comilla', 'Sunday', 800),
-(3, 'Bill Gray', '3', 'Radiotherapist', 'Dhaka', 'Gazipur', 'Friday', 500),
-(4, 'Misty Owens', '4', 'Neuro Surgeon', 'Dhaka', 'Dhaka', 'Monday', 1500),
-(5, 'Sam Barton', '5', 'Orthopedic Surgery', 'Sylhet', 'Habiganj', 'Friday', 1000);
+(1, 'Sofia Brient', '1.jpg', 'Child Specialist', 'Dhaka', 'Dhaka', 'Friday', 500),
+(2, 'Karen Ray', '2.jpg', 'Skin Specialist', 'Chittagong', 'Comilla', 'Sunday', 800),
+(3, 'Bill Gray', '3.jpg', 'Radiotherapist', 'Dhaka', 'Gazipur', 'Friday', 500),
+(4, 'Misty Owens', '4.jpg', 'Neuro Surgeon', 'Dhaka', 'Dhaka', 'Monday', 1500),
+(5, 'Sam Barton', '5.jpg', 'Orthopedic Surgery', 'Sylhet', 'Habiganj', 'Friday', 1000),
+(7, 'Holly R Drew', '6473a5ce59880_female20171026250424011.jpg', 'Cardiac Surgery', 'Chittagong', 'Coxs Bazar', 'Friday', 2000);
 
 -- --------------------------------------------------------
 
@@ -79,7 +80,9 @@ INSERT INTO `user_appoint` (`id`, `user_id`, `dr_id`) VALUES
 (46, 2, 3),
 (47, 2, 3),
 (48, 2, 3),
-(49, 2, 4);
+(49, 2, 4),
+(50, 4, 3),
+(51, 3, 7);
 
 -- --------------------------------------------------------
 
@@ -101,7 +104,9 @@ CREATE TABLE `user_form` (
 
 INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `user_type`) VALUES
 (1, 'admin', 'admin@drsappoint.com', 'c3ca4800424e539f861a51a86b8b13ed', 'admin'),
-(2, 'Nesar Ahmed', 'nadimahmednesar@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'user');
+(2, 'Nesar Ahmed', 'nadimahmednesar@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 'user'),
+(3, 'Nesar Ahmed', 'nadimnesar.cse@gmail.com', '202cb962ac59075b964b07152d234b70', 'user'),
+(4, 'Farhin Ahmed', 'farhin15-13615@diu.edu.bd', '81dc9bdb52d04dc20036dbd8313ed055', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -133,19 +138,19 @@ ALTER TABLE `user_form`
 -- AUTO_INCREMENT for table `dr_list`
 --
 ALTER TABLE `dr_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user_appoint`
 --
 ALTER TABLE `user_appoint`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `user_form`
 --
 ALTER TABLE `user_form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
